c73adc679aac7240592acc6a8c846ad583bef74a
Joseph Chiang <joseph.chiang@whitech.com.au>
2019-05-06 10:05:54 +1000

    Exclude React.Fragement since it's useless and causing warnings (Fix #5)

---
+++ test/fixtures/react-fragment/actual.js
@@ -0,0 +1,11 @@
+React.createClass({
+  render: function() {
+    return (
+      <React.Fragment>
+        <Fragment>
+          Element contents
+        </Fragment>
+      </React.Fragment>
+    );
+  },
+});
---
+++ test/fixtures/react-fragment/expected.js
@@ -0,0 +1,9 @@
+React.createClass({
+  render: function () {
+    return <React.Fragment>
+        <Fragment>
+          Element contents
+        </Fragment>
+      </React.Fragment>;
+  }
+});
